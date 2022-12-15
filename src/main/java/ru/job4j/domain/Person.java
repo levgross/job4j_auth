e362d534cd4f83fbafd875a41aa7c508670f0c03
Grossevich Lev <levgross@gmail.com>
2022-12-15 16:10:46 +0400

    1. RESTFull. Описание архитектуры [#6884]

---
+++ src/main/java/ru/job4j/domain/Person.java
@@ -0,0 +1,19 @@
+package ru.job4j.domain;
+
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private int id;
+    private String login;
+    private String password;
+}
---
+++ src/main/java/ru/job4j/repository/PersonRepository.java
@@ -0,0 +1,12 @@
+package ru.job4j.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import ru.job4j.domain.Person;
+
+import java.util.List;
+
+@Repository
+public interface PersonRepository extends CrudRepository<Person, Integer> {
+    List<Person> findAll();
+}
